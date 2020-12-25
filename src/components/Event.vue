ad9bbf8844e95b3638860711269d52ce592d8f81
Danilo Dimitrievski <dimitrievski.danilo@gmail.com>
2020-12-25 19:08:50 +0100

    Updates

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,34 +1,45 @@
 <template>
   <div class="container d-flex flex-column">
     <el-button @click="onBack" type="primary"> Back </el-button>
-    <h2>{{ event.name.text }}</div>
-    <div class="pb-2 fs-5 text-primary">{{eventCreated(event)}}</div>
+    <h2>{{ event.name.text }}</h2>
+    <div class="pb-2 fs-5 text-primary">{{ eventCreated(event) }}</div>
     <p class="pb-2 lead">
       {{ event.summary }}
     </p>
     <div class="pb-2 fs-4">
-      <span class="text-primary fas fa-clock"></span>{{eventTime(event)}}
+      <span class="text-primary fas fa-clock"></span>{{ eventTime(event) }}
     </div>
 
     <div class="pb-2 fs-4">
-      <span class="text-primary fas fa-globe-europe"></span>{{event.url}}
+      <span class="text-primary fas fa-globe-europe"></span>{{ event.url }}
     </div>
 
     <div class="pb-2 fs-4" v-if="event.venue">
       <div class="pb-2">
-        <span class="text-primary fas fa-map-marker"></span>{{formatAddress(venue.address)}}
+        <span class="text-primary fas fa-map-marker"></span
+        >{{ formatAddress(venue.address) }}
       </div>
     </div>
 
     <div class="pb-2 fs-4" v-else>
       <span class="text-primary fas fa-map-marker"></span>No address listed.
     </div>
+    <h3>Tickets</h3>
+    <div v-for="ticket in event.ticket_classes" :key="ticket.id">
+      <h4>{{ ticket.name }}</h4>
+
+      <div class="d-flex">
+        <div>Capacity: {{ ticket.quantity_total }}</div>
+        <div>Sold: {{ ticket.quantity_sold }}</div>
+        <div>Price: {{ ticket.cost.value }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-import {eventTime, eventCreated} from "../helpers";
+import { eventTime, eventCreated } from "../helpers";
 
 export default {
   data() {
